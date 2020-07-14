3634bafa4777ac2dc62de12ca7f60f1db27fda02
unknown <luketpena@gmail.com>
2020-07-14 17:16:30 -0500

    Version 3.0

--- solution.js
+++ solution.js
@@ -2,14 +2,25 @@
     init: function(elevators, floors) {
         var elevator = elevators[0]; // Let's use the first elevator
         let floorNum = 4;
+        
+        let waitList = -1;
+        
+        for (let floor of floors) {
+            floorListen(floor);
+        }
 
         // Whenever the elevator is idle (has no more queued destinations) ...
         elevator.on("idle", function() {
             let calledFloors = elevator.getPressedFloors();
             if (calledFloors.length>0) {
-                for (let floor of calledFloors) {
+                for (let floor of calledFloors) {                    
                     elevator.goToFloor(floor);
                 }  
+            }
+            
+            if (waitList!==-1) {
+                elevator.goToFloor(floor);
+                waitList = -1;
             }
        
             //Clear the indicators
@@ -21,8 +32,33 @@
             elevator.goToFloor(floorNum);
         });
         
- 
-        
+         
+        function floorListen(floor) {
+            const floorNum = floor.floorNum();
+            const calledFloors = elevator.getPressedFloors();
+            
+            floor.on("up_button_pressed", function() {
+                console.log('UP PRESSED')
+                if (calledFloors.length>0) {
+                    if (floorNum>elevator.currentFloor() && elevator.destinationDirection()==="up") {
+                        elevator.goToFloor(floorNum);
+                    } else if (waitList===-1) {
+                        waitList = floorNum;
+                    } 
+                } else elevator.goToFloor(floorNum);
+            });
+
+            floor.on("down_button_pressed", function() {
+                if (calledFloors.length>0) {
+                    if (floorNum<elevator.currentFloor() && elevator.destinationDirection()==="down") {
+                        elevator.goToFloor(floorNum);
+                    } else if (waitList===-1) {
+                        waitList = floorNum;
+                    }
+                } else elevator.goToFloor(floorNum);
+            });
+
+        }
 
     },
     update: function(dt, elevators, floors) {
